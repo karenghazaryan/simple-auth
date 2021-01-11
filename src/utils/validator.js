ade9fa3dff79207fcd23a2df40c2fb9a3f721710
karen <karghazaryankar@gmail.com>
2021-01-11 19:14:47 +0400

    Fixed Name validation

--- src/utils/validator.js
+++ src/utils/validator.js
@@ -8,7 +8,7 @@ function validatePass(pass) {
 }
 
 function validateName(name) {
-     const reg = /^[a-z,',-]+(\s)[a-z,',-]+$/
+     const reg = /^([a-zA-Z0-9]+|[a-zA-Z0-9]+\s{1}[a-zA-Z0-9]{1,}|[a-zA-Z0-9]+\s{1}[a-zA-Z0-9]{3,}\s{1}[a-zA-Z0-9]{1,})$/
      return reg.test(name);
 }
 
